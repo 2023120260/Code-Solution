3f8451603a1cfba1a6e13b596736eb489ae3c440
Dio_Bolo <94669720+2023120260@users.noreply.github.com>
2024-03-31 14:24:28 +0800

    Update 力扣206.反转链表.cpp

--- 力扣206.反转链表.cpp
+++ 力扣206.反转链表.cpp
@@ -8,6 +8,9 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//把C++捡回来写的第一道题，用的头插法暴力解了，可以优化很多，但是先交了。
+//后面再想进阶
+//进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
